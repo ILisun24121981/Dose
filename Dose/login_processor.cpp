446ef8739bda2395d90588229e36d25642125a5b
Ivan <LisunIvan@gmail.com>
2020-02-07 05:14:15 +0300

    Сделал чтение из файла построчное

--- Dose/login_processor.cpp
+++ Dose/login_processor.cpp
@@ -1,6 +1,6 @@
 #include "login_processor.h"
 #include <QDebug>
-
+#include <QFile>
 
 Lis::Login_processor::Login_processor(Controller *ct):Processor(ct){
 
@@ -13,6 +13,16 @@ Lis::Login_processor::~Login_processor(){
 void Lis::Login_processor::loginCheck(QString username,QString password) {
     qDebug()<<username;
     qDebug()<<password;
+
+    QFile loginfile ("./Login/login.txt");
+    if (!loginfile.open(QIODevice::ReadOnly | QIODevice::Text))
+        return;
+    qDebug()<<"file opened";
+    QTextStream in(&loginfile);
+    while (!in.atEnd()) {
+        QString line = in.readLine();
+        qDebug()<<line;
+    }
 }
 //void Lis::Loginner::connection() {
 //    QObject::connect(_controller->_widget,SIGNAL(login(QString,QString)),_controller->_processor,SLOT(loginCheck(QString,QString)));
